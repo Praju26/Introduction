c3da2f2d40cd403747fd86b328b35c0044d2316e
PRAJAKTA <PRAJAKTA@LAPTOP-DQH2B71H>
2021-08-14 21:52:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool? AreYouMajor = null;
+            if (AreYouMajor == true)
+            {
+                Console.WriteLine("User is Major");
+            }
+            else if (AreYouMajor == false) 
+            {
+                Console.WriteLine("User is not Major");
+            }
+        
+            else
+            {
+                Console.WriteLine("User is Did not answer the question");
+            }
+        }
+    }
+}
